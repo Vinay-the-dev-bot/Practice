4ea93a8aa76347b40efd77d67614cf2aaf0eb302
Vinaygouda Meti <57762023+Vinay-the-dev-bot@users.noreply.github.com>
2022-02-03 14:44:24 +0530

    Add files via upload

---
+++ HTML/Project1.js
@@ -0,0 +1,68 @@
+function myFunction(x) {
+    x.classList.toggle("change");
+  }
+
+  // var coll = document.getElementsByClassName(""); 
+  const coll = document.getElementsByClassName("collabutton");
+  console.log("Coll : " + coll.length);
+  var i;
+  
+  for (i = 0; i < coll.length; i++) {
+    coll[i].addEventListener("click", function() {
+      this.classList.toggle("active");
+      var collabutton = this.nextElementSibling;
+      if (collabutton.style.display === "block") {
+        collabutton.style.display = "none";
+        collabutton.style.maxHeight = "100px";
+      
+      } else {
+        collabutton.style.display = "block";
+        collabutton.style.maxHeight = collabutton.scrollHeight+"px";
+      }
+       
+    });
+  }
+
+
+const tabs = document.querySelectorAll("[data-tab-target]")
+const tabcontents = document.querySelectorAll(".panelsss") 
+
+tabs.forEach(tab => {
+   
+  tab.addEventListener("click", () =>{
+    const target = document.querySelector(tab.dataset.tabTarget)
+    tabcontents.forEach(tabcontent =>{
+      tabcontent.classList.remove("active1")
+    })
+    target.classList.add('active1')
+  })
+})
+ 
+
+const menu = document.getElementById("MenuPanel22") 
+menu.addEventListener("click", function(){ 
+
+      if(getComputedStyle(menu).backgroundColor === "rgba(0, 0, 0, 0)"){ 
+        menu.style.backgroundColor ="gray";
+        menu.style.height= "160px"; 
+        menu.style.borderRadius="10px" ; 
+      }
+      else{
+        menu.style.backgroundColor ="rgba(0, 0, 0, 0)";
+        menu.style.height= ""; 
+      }
+
+      const collabutton1 =  document.getElementById("MenuList1")  
+        if ( getComputedStyle(collabutton1).display === "none") { 
+          collabutton1.style.display="block"; 
+      } else   {   
+        collabutton1.style.display="none";  
+      } 
+      const menupanelbar22 =  document.getElementById("menupanelbar22")   
+      if ( getComputedStyle(menupanelbar22).opacity === "1") {
+        menu.style.paddingLeft = "100px"; 
+        
+      } else  {  
+        menu.style.paddingLeft = "0";  
+      }
+}) 
